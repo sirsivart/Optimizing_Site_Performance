f6c9705715411f734628fb3e63bbebdf85c34bd8
travis carney <traviscarney@gmail.com>
2017-08-11 22:13:04 -0400

    Have Gulp working if ran twice
    I have to run gulp twice for it to work. The first run through it creates the app.css and app.js files the next run through it creates the app.min.css and app.min.js files. As of right now it is down to 28 requests.

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+"use strict";
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    maps = require('gulp-sourcemaps'),
+    del = require('del'),
+    clean = require('gulp-clean-css');
+
+gulp.task("concatScripts", function() {
+    gulp.src([
+            'js/jquery.js',
+            'js/fastclick.js',
+            'js/foundation.js',
+            'js/foundation.equalizer.js',
+            'js/foundation.reveal.js',
+            'js/scripts.js'
+        ])
+        .pipe(maps.init())
+        .pipe(concat('app.js'))
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('js'));
+
+        gulp.src([
+  	'css/normalize.css',
+  	'css/foundation.css',
+  	'css/basics.css',
+  	'css/menu.css',
+  	'css/hero.css',
+  	'css/photo-grid.css',
+  	'css/modals.css',
+  	'css/footer.css'
+  	])
+        .pipe(maps.init())
+        .pipe(concat('app.css'))
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task("minifyScripts", ["concatScripts"], function() {
+   return gulp.src("js/app.js")
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('js'));
+});
+
+// gulp.task("concatScripts", function() {
+//     gulp.src([
+//   	'css/normalize.css',
+//   	'css/foundation.css',
+//   	'css/basics.css',
+//   	'css/menu.css',
+//   	'css/hero.css',
+//   	'css/photo-grid.css',
+//   	'css/modals.css',
+//   	'css/footer.css'
+//   	])
+//         .pipe(maps.init())
+//         .pipe(concat('app.css'))
+//         .pipe(maps.write('./'))
+//         .pipe(gulp.dest('css'));
+// });
+
+gulp.task('minify-css', ["concatScripts"], () => {
+   return gulp.src(["css/app.css"])
+    .pipe(maps.init())
+    .pipe(clean())
+    .pipe(maps.write())
+    .pipe(rename('app.min.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task("build", ['minifyScripts', 'minify-css'], function() {
+    return gulp.src(["css/app.min.css", "js/app.min.js", 'index.html',
+            "img/**"
+        ], {
+            base: './'
+        })
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task("default", ["build"]);
